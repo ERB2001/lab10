f81e10f58560053ca8120e330c0265d4d989e9ba
Elia Rossi Baroncelli <elia.rossibaroncelli@studio.unibo.it>
2022-12-04 17:53:54 +0100

    After the exercise but the last method I had to take almost completely from the solutions. Writing instructions in the enumeration taken from profs (Exercise 103)

--- 103-stream-utilities-for-text-processing/src/main/java/it/unibo/oop/lab/streams/LambdaFilter.java
+++ 103-stream-utilities-for-text-processing/src/main/java/it/unibo/oop/lab/streams/LambdaFilter.java
@@ -7,8 +7,13 @@ import java.awt.GridLayout;
 import java.awt.LayoutManager;
 import java.awt.Toolkit;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import javax.swing.BorderFactory;
@@ -41,11 +46,31 @@ public final class LambdaFilter extends JFrame {
 
     // private final static String NEWLINE = "\n";
 
+    private static final String ANY_NON_WORD = "(\\s|\\p{Punct})+";
+
     private enum Command {
         /**
          * Commands.
          */
-        IDENTITY("No modifications", Function.identity());
+        IDENTITY("No modifications", Function.identity()),
+
+        TO_LOWER("Converter to lowercase", s -> s.toLowerCase()),
+
+        CHARS_NUMBER("Count the number of chars", (s -> Long.toString(s.chars().count()))),
+
+        LINES_NUMBER("Count the number of lines", (s -> Long.toString(s.lines().count()))),
+
+        ALPHABETICAL_ORDER("List all the words in alphabetical order",
+                (s -> Arrays
+                        .stream(s.split(ANY_NON_WORD))
+                        .sorted()
+                        .collect(Collectors.joining()))),
+
+        WORDS_COUNT("Write the count for each word",
+                (s -> Arrays.stream(s.split(ANY_NON_WORD))
+                        .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                        .entrySet().stream()
+                        .map(e -> e.getKey() + e.getValue()).collect(Collectors.joining())));
 
         private final String commandName;
         private final Function<String, String> fun;
@@ -63,14 +88,6 @@ public final class LambdaFilter extends JFrame {
         public String translate(final String s) {
             return fun.apply(s);
         }
-
-        /*
-         * public Stream<String> toLowerCase(final String s) {
-         * List<String> lowerCase =
-         * list.stream().map(String::toLowerCase).collect(Collectors.toList());
-         * return null;
-         * }
-         */
     }
 
     private LambdaFilter() {
@@ -95,16 +112,73 @@ public final class LambdaFilter extends JFrame {
         // combo.getSelectedItem()).translate(left.getText())));
         // new Thread(() -> right.setText)
         apply.addActionListener(ev -> {
-            // right.setText(((Command) combo.getSelectedItem()).translate(left.getText()));
-            List<String> list = new ArrayList<String>();
+            right.setText(((Command) combo.getSelectedItem()).translate(left.getText()));
 
-            list.add(left.getText().toLowerCase());
+            // TENTATIVI MIEI
+            // List<String> list = new ArrayList<String>();
 
-            list.add(String.valueOf(left.getText().chars().filter(ch -> ch != ' ').filter(ch -> ch != '\n').count()));
+            // list.add(left.getText().toLowerCase()); BUONE
 
-            list.add(String.valueOf(left.getText().lines().count()));
+            // list.add(String.valueOf(left.getText().chars().filter(ch -> ch != '
+            // ').filter(ch -> ch != '\n').count())); BUONE
 
-            right.append(list.toString());
+            // list.add(String.valueOf(left.getText().lines().count())); BUONE
+
+            // right.setText(list.stream().sorted().toString()).translate(left.getText().toString());
+
+            // right.setText(List.of(left.getText()).stream().sorted().toString());
+
+            // right.setText(List.of(left.getText()).stream().sorted().toString());
+            // System.out.println(List.of(left.getText().toString()).sort(s -> s));
+            // String cazzo = left.getText();
+            // list.add(cazzo);
+            // System.out.println(list);
+
+            // list.add(list2.toString());
+
+            // String output = left.getText().substring(0).toString();
+
+            // List<IntStream> list2 = Stream.of(right.getText().chars().filter(ch -> ch !=
+            // ' ' && ch != '\n')).sorted().toList();
+
+            // List<String> listTMP = list.sort(String::compareTo);
+
+            // list.add(List.of(left.getText()).stream().map(s ->
+            // s.toLowerCase()).sorted().collect(Collectors.joining()));
+
+            // list.add(left.getText().substring(0).compareTo(list.iterator().next().toString()));
+
+            // listTMP = ;
+            // List<String> list3 =
+
+            // Long tmp = list.stream().count();
+
+            // System.out.println(tmp);
+
+            // CERCARE COME METTERE I ORDINE LE STRINGHE O GLI ARRAY DI CARATTERI
+            // MALE MALE MALE MALE MALE
+            // MANCANO UNLTIMI DUE METODI
+
+            // char[] line = left.getText().toCharArray();
+
+            // list.add((Arrays.sort(line)));
+
+            // Arrays.sort(Integer.valueOf(list.add(left.getText().toCharArray().toString())));
+
+            // list.add(List.of(left).stream().sorted().toString());
+
+            // List<String> alphabetical =
+
+            // List.of(left.getText()).stream().sorted().collect(Collectors.toList());
+            // Funziona ma male -- NON ORDINA
+
+            // list.add(((Collection<String>)
+            // left).stream().sorted().collect(Collectors.joining()));
+
+            // list.stream().sorted();
+
+            // right.append(list.toString()); BUONO
+            // right.append(list.stream().sorted().toString());
 
             // right.setText(String.valueOf(left.getText().chars().filter(ch -> ch != '
             // ').count()));
